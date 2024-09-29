d98e7845bf6bc0d8a1bc4d8e4c8522fb5ffa8cd5
Prajugaikwad <siyapraj123@gmail.com>
2024-09-29 18:13:48 +0530

    deletd premiumDetails

--- SpringBootUserPolicyPremium/src/main/java/com/velocity/repository/PolicyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.velocity.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.velocity.model.Policy;
-
-public interface PolicyRepository extends JpaRepository<Policy, Long> {
-}
--- SpringBootUserPolicyPremium/src/main/java/com/velocity/repository/PremiumRepository.java
+++
@@ -1,8 +0,0 @@
-package com.velocity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.velocity.model.Premium;
-
-public interface PremiumRepository extends JpaRepository<Premium, Long> {
-}
