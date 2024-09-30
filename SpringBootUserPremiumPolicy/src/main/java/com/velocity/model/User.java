998839e7e78d4d08647a9ee37f36723b03718fb5
Prajugaikwad <siyapraj123@gmail.com>
2024-09-30 15:58:01 +0530

    SpringBootUserPolicyPremiumDetails

---
+++ SpringBootUserPremiumPolicy/src/main/java/com/velocity/model/User.java
@@ -0,0 +1,78 @@
+package com.velocity.model;
+
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+
+    private Long userId;
+
+    @Column(nullable = false, unique = true)
+
+    private String username;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Policy>policies;
+
+ // Getters and setters for all fields
+    
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Policy> getPolicies() {
+		return policies;
+	}
+
+	public void setPolicies(List<Policy> policies) {
+		this.policies = policies;
+	}
+ 
+}
